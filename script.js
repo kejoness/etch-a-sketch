f43925801033a419e8a49849e70ed6e796385f99
kejoness <kjones9843@gmail.com>
2026-02-06 10:25:11 -0500

    create styles for square divs

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 const container = document.querySelector('#container');
 
 // creates a 16x16 grid
-for(i = 0; i <= 256; i++) {
+let counter = 0;
+while(counter <= 256) {
     square = document.createElement("div");
+    square.style.cssText = "background-color: #d9d9d9; border-color: black; border-width: 3px; display: flex; width: 50px; height: 50px;";
     container.appendChild(square);
+
+    counter++;
 }
