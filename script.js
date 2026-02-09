fae3b8395317a26ae98947587203c4a8581d75ea
kejoness <kjones9843@gmail.com>
2026-02-09 10:01:50 -0500

    generate 16 rows of square divs

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 const container = document.querySelector('#container');
 
 // creates a 16x16 grid
-let counter = 0;
-while(counter <= 256) {
-    square = document.createElement("div");
-    square.style.cssText = "background-color: #d9d9d9; border-color: black; border-width: 3px; display: flex; width: 50px; height: 50px;";
-    container.appendChild(square);
-
+counter = 0;
+while(counter <= 58) {
+    for(row = 0; row < 16; row++) {
+        let row = document.createElement("div");
+        for(cell = 1; cell < 16; cell++) {
+            let cell = document.createElement("div");
+            row.appendChild(cell);
+        }
+        row.style.cssText = "border-width: 1px; border-style: solid; height: 30px; width: 30px; background-color: #b6b8b0;";
+        container.appendChild(row);
+    }
     counter++;
 }
