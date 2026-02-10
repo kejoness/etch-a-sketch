88ab416c61112762f56ce62be54cb4902327d38c
kejoness <kjones9843@gmail.com>
2026-02-10 09:21:21 -0500

    generate correct number of cells within rows

--- script.js
+++ script.js
@@ -2,15 +2,16 @@ const container = document.querySelector('#container');
 
 // creates a 16x16 grid
 counter = 0;
-while(counter <= 58) {
-    for(row = 0; row < 16; row++) {
-        let row = document.createElement("div");
-        for(cell = 1; cell < 16; cell++) {
-            let cell = document.createElement("div");
-            row.appendChild(cell);
-        }
-        row.style.cssText = "border-width: 1px; border-style: solid; height: 30px; width: 30px; background-color: #b6b8b0;";
-        container.appendChild(row);
-    }
-    counter++;
+while(counter <= 7) {
+   for(row = 0; row <= 1; row++) {
+       let row = document.createElement("div");
+       for(cell = 0; cell < 16; cell++) {
+           let cell = document.createElement("div");
+           cell.style.cssText = "border-width: 1px; border-style: solid; height: 30px; width: 30px; background-color: #b6b8b0;";
+           row.appendChild(cell);
+       }
+       row.style.cssText = "border-width: 1px; border-style: solid; height: 30px; width: 30px; background-color: #b6b8b0;";
+       container.appendChild(row);
+   }
+   counter++;
 }
